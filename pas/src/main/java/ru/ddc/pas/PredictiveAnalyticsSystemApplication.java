0af77ae2ff993fdd21a9e5b067ed8813f9e58154
Sergey Kovalev <dipdeepcode@mail.ru>
2023-05-21 13:09:44 +0300

    Добавлен модуль pas

---
+++ pas/src/main/java/ru/ddc/pas/PredictiveAnalyticsSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.pas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictiveAnalyticsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictiveAnalyticsSystemApplication.class, args);
+	}
+
+}
